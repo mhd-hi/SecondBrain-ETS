7f78980255f3776a2ec699d7474529252412da2f
Mohamed Idrissi <mhdhafdi2@gmail.com>
2025-09-21 05:03:41 -0400

    remove vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Analytics } from '@vercel/analytics/next';
 import { SessionProvider } from 'next-auth/react';
 import { Geist, Inter } from 'next/font/google';
 import { GlobalConfirmDialogProvider } from '@/components/shared/dialogs/ConfirmDialogProvider';
@@ -41,7 +40,6 @@ export default function RootLayout({
               <PomodoroProvider>
                 {children}
                 <Toaster />
-                <Analytics />
               </PomodoroProvider>
             </ThemeProvider>
           </GlobalConfirmDialogProvider>
