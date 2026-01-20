a393f4e1b0c456efee6ba1850512032761b2b515
Mohamed Idrissi <mhdhafdi2@gmail.com>
2026-01-19 23:19:30 -0500

    trigger db ci

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -16,13 +16,6 @@ import {
   uuid,
 } from 'drizzle-orm/pg-core';
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-
 // NextAuth.js user and authentication tables
 export const users = pgTable('user', {
   id: text('id')
